1212e85bfccb2e4f3cee8cdc152148605d47da7d
Calizard <119135039+Calizard@users.noreply.github.com>
2024-03-04 13:08:28 -0500

    Update question2.c

--- question2.c
+++ question2.c
@@ -8,10 +8,37 @@
 #define _XOPEN_SOURCE 700 // required for barriers to work
 #include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
+    pid_t pid;
 
+    // Fork a child process
+    pid = fork();
+    // Error case for failing fork
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process
+        sleep(1);
+        printf("Child process\n");
+        exit(EXIT_SUCCESS);
+    } else {
+        // Parent process
+        int status;
+        wait(&status); // Wait for the child process to terminate
+
+        if (WIFEXITED(status)) {
+            printf("Child process exited with status: %d\n", WEXITSTATUS(status));
+        } else {
+            printf("Child process terminated abnormally\n");
+        }
+
+        printf("Parent Process\n");
+    }
+
+    return 0;
 }
